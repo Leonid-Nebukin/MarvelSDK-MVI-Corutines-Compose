67c332fcfca7c94b653ef7835ebcb3ced687527c
Leonid <inextremo97@gmail.com>
2021-09-19 22:12:36 +0300

    add architecture

--- app/src/main/java/com/example/marvelcompose/data/model/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelcompose/data/model/CharacterResponse.kt
@@ -4,6 +4,25 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class CharacterResponse (
-    @Json(name = "copyright") val copyright: String
+data class CharacterResponse(
+    @Json(name = "data") val data: Data
+)
+
+@JsonClass(generateAdapter = true)
+data class Data(
+    @Json(name = "results") val results: Results
+)
+
+@JsonClass(generateAdapter = true)
+data class Results(
+    @Json(name = "id") val id: Int,
+    @Json(name = "name") val name: String,
+    @Json(name = "description") val description: String,
+    @Json(name = "thumbnail") val thumbnail: Thumbnail
+)
+
+@JsonClass(generateAdapter = true)
+data class Thumbnail(
+    @Json(name = "path") val path: String,
+    @Json(name = "extension") val extension: String
 )
